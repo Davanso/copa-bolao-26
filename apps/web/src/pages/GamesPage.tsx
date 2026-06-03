9fe150e3efb6409a202b49a9f80b569c8583532b
davanso <guipvp234@gmail.com>
2026-06-03 20:02:15 -0300

    style: fix typo

--- apps/web/src/pages/GamesPage.tsx
+++ apps/web/src/pages/GamesPage.tsx
@@ -376,7 +376,7 @@ const stageTabsConfig = [
   },
   {
     id: "third",
-    label: "3? lugar",
+    label: "3 lugar",
     match: (game: Game) => game.stage === "Disputa de terceiro lugar",
   },
 ];
